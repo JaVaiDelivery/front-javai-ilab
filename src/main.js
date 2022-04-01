faab65c242e2ed1473459a5ff9d532d2aaf5a7de
TalitaCarvalho <talitadealmeidacarvalho9@outlook.com>
2022-04-01 09:35:58 -0300

    feat: implementação do mapa

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGoogleMaps from '@fawmi/vue-google-maps' 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
 const app = createApp(App)
 app.use(router)
+app.use(VueGoogleMaps, {
+    load: {
+        key: import.meta.env.VITE_MAPS_KEY
+    }
+})
 app.mount('#app')
+
--- src/pages/RastreamentoPedidoPages.vue
+++ src/pages/RastreamentoPedidoPages.vue
@@ -1,90 +1,132 @@
 <template>
-  <div
-    class="modal fade"
-    id="infoModal"
-    data-bs-backdrop="static"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content p-3">
-        <div class="modal-body text-center">
-          <h5 class="fw-bold">Entrega {{ modalTitle }}</h5>
-          <div>Voltar para tela inicial</div>
-        </div>
+  <div class="top-0 start-0">
+    <div
+      class="modal fade"
+      id="infoModal"
+      data-bs-backdrop="static"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content p-3">
+          <div class="modal-body text-center">
+            <h5 class="fw-bold">Entrega {{ modalTitle }}</h5>
+            <div>Voltar para tela inicial</div>
+          </div>
 
+          <button
+            type="button"
+            class="btn btn-primary mx-auto"
+            @click="backToHome"
+          >
+            Voltar
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="
+        position-absolute
+        top-0
+        start-50
+        translate-middle-x
+        bg-white
+        px-4
+        py-2
+      "
+      style="z-index: 2; border-radius: 12px; width: 90%; margin-top: 4.5rem"
+    >
+      <div class="mb-3">
+        <div class="fw-bold">Cliente</div>
+        <div class="text-muted" style="font-size: 0.9rem">
+          {{ pedido.cliente.nome }}
+        </div>
+      </div>
+
+      <div>
+        <div class="fw-bold">Endereço</div>
+        <div class="text-muted" style="font-size: 0.9rem">
+          {{ pedido.cliente.endereco }}
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="position-absolute bottom-0 start-50 translate-middle-x"
+      style="z-index: 2"
+    >
+      <div class="mb-5 d-grid gap-5 d-flex justify-content-evenly mt-5 mx-4">
         <button
           type="button"
-          class="btn btn-primary mx-auto"
-   
-          @click="backToHome"
+          class="col btn btn-danger btn"
+          @click="cancelarEntrega"
         >
-          Voltar
+          Cancelar
+        </button>
+        <button
+          type="button"
+          class="col btn btn-success btn"
+          @click="concluirEntrega"
+        >
+          Concluir
         </button>
       </div>
     </div>
   </div>
 
-  <div class="mb-3 mt-5">
-    <div class="fw-bold">Cliente</div>
-    <div class="text-muted" style="font-size: 0.9rem">
-      {{ pedido.cliente.nome }}
-    </div>
-  </div>
-  <div class="mb-3">
-    <div class="fw-bold">Endereço</div>
-    <div class="text-muted" style="font-size: 0.9rem">
-      {{ pedido.cliente.endereco }}
-    </div>
-  </div>
-  <div class="text-center mt-5">
-    <img src="../assets/moto.png" alt="entregador" style="width: 60%" />
-  </div>
-  <div class="d-grid gap-5 d-flex justify-content-evenly mt-5 mx-4">
-    <button
-      type="button"
-      class="col btn btn-danger btn"
-      @click="cancelarEntrega"
-    >
-      Cancelar
-    </button>
-    <button
-      type="button"
-      class="col btn btn-success btn"
-      @click="concluirEntrega"
-    >
-      Concluir
-    </button>
-  </div>
+  <GMapMap
+    v-if="mapCenter"
+    :center="mapCenter"
+    :zoom="14"
+    :disableDefaultUI="true"
+    map-type-id="terrain"
+  >
+    <GMapMarker :position="mapCenter" />
+
+  </GMapMap>
 </template>
 
 <script>
 import store from "../store";
 import PedidoController from "../controllers/PedidoController";
-import { Modal } from 'bootstrap'
+import { Modal } from "bootstrap";
+import { decode } from "@googlemaps/polyline-codec";
 
 export default {
   created() {
+    // const decoded = decode(
+    //   "`|lVb_jjF_AQVmAFY}E_AmFeAgFkAACCCGAI@kFaAuDu@wD{@aF}@?A?AAA\\wALo@QEkCk@",
+    //   5
+    // );
+    // const points = decoded.map(([lat, lng]) => ({ lat, lng }));
+    // console.log(points);
+    // this.paths = points
     this.watchId = navigator.geolocation.watchPosition(this.pegarPosicoes);
   },
 
-  mounted () {
-    this.modalController = new Modal(document.getElementById('infoModal'))
+  mounted() {
+    this.modalController = new Modal(document.getElementById("infoModal"));
   },
 
   data() {
     return {
       pedido: store.state.pedido,
       entregador: store.state.user.data,
+      paths: [],
+      mapCenter: null,
       watchId: null,
       modalController: null,
-      modalTitle: null
+      modalTitle: null,
     };
   },
 
   methods: {
     pegarPosicoes(position) {
+      this.mapCenter = {};
+      this.mapCenter.lat = position.coords.latitude;
+      this.mapCenter.lng = position.coords.longitude;
       const posicao = {
         coordenadas: `${position.coords.latitude} / ${position.coords.longitude}`,
         entregador: {
@@ -99,16 +141,16 @@ export default {
       PedidoController.enviarLocalizacao(posicao);
     },
     async cancelarEntrega() {
-      navigator.geolocation.clearWatch(this.watchId);
-      const response = await PedidoController.controlarEntrega(
-        this.pedido.id,
-        this.entregador.id_entregador,
-        0
-      );
-      if (response.success) {
-        this.modalTitle = 'Cancelada'
-        this.modalController.show()
-      }
+      // navigator.geolocation.clearWatch(this.watchId);
+      // const response = await PedidoController.controlarEntrega(
+      //   this.pedido.id,
+      //   this.entregador.id_entregador,
+      //   0
+      // );
+      // if (response.success) {
+      //   }
+      this.modalTitle = "Cancelada";
+      this.modalController.show();
     },
     async concluirEntrega() {
       navigator.geolocation.clearWatch(this.watchId);
@@ -118,22 +160,20 @@ export default {
         2
       );
       if (response.success) {
-        this.modalTitle = 'Concluída'
-        this.modalController.show()
+        this.modalTitle = "Concluída";
+        this.modalController.show();
       }
     },
     backToHome() {
-      this.modalController.hide()
-      this.$router.replace({ name: 'pedidos' })
-    }
+      this.modalController.hide();
+      this.$router.replace({ name: "pedidos" });
+    },
   },
 };
 </script>
 
 <style scoped>
-.cancel-modal {
-  background-color: rgba(0, 0, 0, 0.5);
-  width: 100%;
-  height: 100%;
+.vue-map-container {
+  height: calc(100vh - 56px);
 }
 </style>
